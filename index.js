2e8b61ec81bf3c36d59c3233a91f718d360bd2f9
关签 <buzichang@vip.qq.com>
2017-03-31 20:19:01 +0800

    foo

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-var loader = require('./lib/core/loader')
-var command_download = require('./lib/command/download')
-
-module.exports.getLoader = function (name) {
-    return loader[name]
-}
-
-module.exports.getLoaderFromUrl = function (url) {
-    return command_download.parseUrl(url)
+module.exports.foo = function () {
 }
